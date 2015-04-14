235acaa7d99a8fe9b3b1f0ed9bea5f805db1f2bc
Mark Pokorny <iridium193@gmail.com>
2015-04-14 12:59:46 +0100

    Now tests for increase and decrease in temperature.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,7 +1,21 @@
 describe('Thermostat', function() {
-  var thermostat = new Thermostat();
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   it('starts at 20 degrees CELSIUS', function() {
     expect(thermostat.temp).toEqual(20);
+  });
+
+  it('increases the temperature upon request', function() {
+    thermostat.increaseTemp();
+    expect(thermostat.temp).toEqual(21);
+  });
+
+  it('decreases the temperature upon request', function() {
+    thermostat.decreaseTemp();
+    expect(thermostat.temp).toEqual(19);
   });
 });
