abf600dc15418bda087917bb08318b0454bbb5af
Mark Pokorny <iridium193@gmail.com>
2015-04-15 12:03:31 +0100

    Tested for default temperature display on webpage.

---
+++ spec/ThermostatFrontSpec.js
@@ -0,0 +1,13 @@
+describe('Thermostat Front-End', function() {
+
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+  });
+
+  it('contains text 20°C when loaded', function() {
+    expect('#temperature').toContainText('20°C');
+  });
+
+
+});
