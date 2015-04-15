8856c2dff98c6fbfabf29f159f1cb8dca9372b02
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-15 13:08:44 +0100

    Up button fully added and functional

--- src/ThermostatFront.js
+++ src/ThermostatFront.js
@@ -1,2 +1,11 @@
 var thermostat = new Thermostat();
+var updatePage = function() {
 document.getElementById('temperature').innerHTML = thermostat.temp + '°C';
+};
+
+updatePage();
+
+document.getElementById('up').addEventListener('click', function() {
+  thermostat.increaseTemp();
+  updatePage();
+});
